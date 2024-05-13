83ae058dad819744e16816f5197bcc6ad26e92c8
lemorage <one.lemorage@gmail.com>
2024-05-13 16:40:37 +0800

    refactor main program and add command line flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,8 +19,66 @@ type Config struct {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	dir := "./ui/html/pages"
+	// Define command-line flags
+	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
+	port := startCmd.Int("port", 7536, "port number to start the server")
+
+	// Parse command-line arguments
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: sack [start]")
+		os.Exit(1)
+	}
+
+	if os.Args[2] < "1" || os.Args[2] > "65535" {
+		log.Fatalf("Invalid port number: %d. Port number must be between 1 and 65535.", port)
+		os.Exit(1)
+	}
+
+	config, err := readConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	tmpl := parseTemplates()
+
+	generateHTMLFiles(config, tmpl)
+
+	mux := setupHandlers(config)
+
+	switch os.Args[1] {
+	case "start":
+		if len(os.Args) == 2 {
+			startCmd.Parse(os.Args[2:])
+			if startCmd.Parsed() {
+				startServer(mux, *port)
+			}
+		} else {
+			fmt.Println("Usage: sack start [flags]")
+			startCmd.PrintDefaults()
+			os.Exit(1)
+		}
+	default:
+		fmt.Println("Usage: sack [start]")
+		os.Exit(1)
+	}
+
+	// Start server if the "start" command is provided
+	if startCmd.Parsed() {
+		startServer(mux, *port)
+	}
+}
+
+func readConfig(filename string) (Config, error) {
+	var config Config
+	configData, err := os.ReadFile(filename)
+	if err != nil {
+		return config, err
+	}
+	err = yaml.Unmarshal(configData, &config)
+	return config, err
+}
+
+func parseTemplates() *template.Template {
 	funcMap := template.FuncMap{
 		"add": func(i int) int {
 			return i + 1
@@ -28,37 +87,14 @@ func main() {
 			return i - 1
 		},
 	}
+	return template.Must(template.New("base").Funcs(funcMap).ParseGlob("ui/html/*.gohtml"))
+}
 
-	configData, err := os.ReadFile("config.yaml")
-	if err != nil {
-		log.Fatalf("Error reading config file: %s", err)
-	}
-
-	var config Config
-	err = yaml.Unmarshal(configData, &config)
-	if err != nil {
-		log.Fatalf("Error parsing config YAML: %s", err)
-	}
-
-	tmpl := template.Must(template.New("base").Funcs(funcMap).ParseGlob("ui/html/*.gohtml"))
-	file, err := os.Open("ui/html/base.gohtml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// Sort the keys of config.Pages
-	var keys []string
-	for key := range config.Pages {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	i := 1
-
-	for _, page := range keys {
+func generateHTMLFiles(config Config, tmpl *template.Template) {
+	dir := "./ui/html/pages"
+	for i, page := range sortedPageKeys(config.Pages) {
 		pageConfig := config.Pages[page]
-		pageFilename := filepath.Join(dir, fmt.Sprintf("page%d.gohtml", i))
+		pageFilename := filepath.Join(dir, fmt.Sprintf("page%d.gohtml", i+1))
 		newPage, err := os.Create(pageFilename)
 		if err != nil {
 			log.Fatalf("Error creating page file for %s: %s", page, err)
@@ -70,27 +106,48 @@ func main() {
 			TotalPages  int
 			PageConfig  PageConfig
 		}{
-			CurrentPage: i,
+			CurrentPage: i + 1,
 			TotalPages:  len(config.Pages),
 			PageConfig:  pageConfig,
 		})
 		if err != nil {
 			log.Fatalf("Error executing template for page %s: %s", page, err)
 		}
+		log.Printf("Generated HTML for %s\n", page)
+	}
+}
+
+func sortedPageKeys(pages map[string]PageConfig) []string {
+	keys := make([]string, 0, len(pages))
+	for key := range pages {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func setupHandlers(config Config) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	for i := 1; i <= len(config.Pages); i++ {
+		pageFilename := fmt.Sprintf("./ui/html/pages/page%d.gohtml", i)
 		mux.HandleFunc("/model"+strconv.Itoa(i), func(w http.ResponseWriter, r *http.Request) {
 			http.ServeFile(w, r, pageFilename)
 		})
-		log.Printf("Generated HTML for %s\n", page)
-		i++
 	}
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	mux.HandleFunc("/", home)
+	return mux
+}
 
-	log.Print("Starting server on :7536")
-	err = http.ListenAndServe(":7536", mux)
-	log.Fatal(err)
+func startServer(mux *http.ServeMux, port int) {
+	addr := fmt.Sprintf(":%d", port)
+	log.Printf("Starting server on %s...\n", addr)
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
 }
