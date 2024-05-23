80faad42d57e9791fdb691812432e27abba36c16
lemorage <one.lemorage@gmail.com>
2024-05-23 16:00:58 +0800

    chore: Add info popup to model-viewer UI

---
+++ ui/static/popup-info.js
@@ -0,0 +1,8 @@
+function togglePopup() {
+  const popup = document.getElementById('popup');
+  if (popup.style.display === 'block') {
+      popup.style.display = 'none';
+  } else {
+      popup.style.display = 'block';
+  }
+}
