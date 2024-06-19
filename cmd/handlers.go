218c3127973839f55ce53ffddb4255beadb1353e
lemorage <one.lemorage@gmail.com>
2024-06-19 11:34:36 +0800

    chore: Add custom error pages and handlers for 404 and 500 errors

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -6,22 +6,52 @@ import (
 	"net/http"
 )
 
+// home handler for the home page
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		notFound(w)
 		return
 	}
 
 	ts, err := template.ParseFiles("./ui/html/index.html")
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		serverError(w, err)
+	}
+}
+
+// notFound handler for custom 404 page
+func notFound(w http.ResponseWriter) {
+	ts, err := template.ParseFiles("./ui/html/404.html")
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	err = ts.Execute(w, nil)
+	if err != nil {
+		serverError(w, err)
+	}
+}
+
+// serverError handler for custom 500 page
+func serverError(w http.ResponseWriter, err error) {
+	log.Print(err.Error())
+	ts, err := template.ParseFiles("./ui/html/500.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+	err = ts.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
